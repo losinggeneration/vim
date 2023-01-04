6583b65fa8ea4be9b2c50f2cd834de60ac91d52f
Harley Laue <losinggeneration@gmail.com>
2023-01-04 15:32:27 -0800

    Add some comments to null-ls for various sources
    * Expand the golangci_lint to run all
    * Reordered diagnostics so eslint is in the correct spot
    * Added yamllint diagnostics
    * Added prettierd formatting

--- .vim/lua/cfg/null-ls.lua
+++ .vim/lua/cfg/null-ls.lua
@@ -1,19 +1,39 @@
 local null_ls = require("null-ls")
+
 null_ls.setup({
-	debug = true,
+	debug = false,
 	sources = {
+		-- Code Actions
 		null_ls.builtins.code_actions.refactoring,
 		null_ls.builtins.code_actions.shellcheck,
 		--null_ls.builtins.code_actions.gitsigns, -- Git actions at position
 		null_ls.builtins.code_actions.eslint,
 
+		-- Completion
 		null_ls.builtins.completion.luasnip, -- luasnip completion
 		null_ls.builtins.completion.tags, -- completion for tags
 		null_ls.builtins.completion.spell, -- completion for spelling
 
+		-- Linters
 		--null_ls.builtins.diagnostics.cspell, -- code spell checker
+		null_ls.builtins.diagnostics.eslint, -- Typescript check with eslint
 		null_ls.builtins.diagnostics.gitlint, -- Git commit messages linter
-		null_ls.builtins.diagnostics.golangci_lint, -- Go Lint
+		null_ls.builtins.diagnostics.golangci_lint.with({
+			args = function(params)
+				return {
+					"run",
+					params.bufname,
+					"--fix=false",
+					"--enable-all",
+					"--fast",
+					"--out-format=json",
+					"--path-prefix",
+					"$ROOT",
+					"--max-issues-per-linter",
+					"0",
+				}
+			end,
+		}), -- Go Lint
 		null_ls.builtins.diagnostics.hadolint, -- Dockerfile best practices
 		null_ls.builtins.diagnostics.jsonlint, -- JSON lint
 		--null_ls.builtins.diagnostics.luacheck, -- Lua linter
@@ -22,20 +42,22 @@ null_ls.setup({
 		null_ls.builtins.diagnostics.staticcheck, -- Go linter
 		null_ls.builtins.diagnostics.teal, -- Teal linter
 		null_ls.builtins.diagnostics.tsc, -- Typescript check with the compiler
+		null_ls.builtins.diagnostics.yamllint,
 		null_ls.builtins.diagnostics.zsh,
-		null_ls.builtins.diagnostics.eslint, -- Typescript check with eslint
 
+		-- Formatters
 		null_ls.builtins.formatting.asmfmt,
 		null_ls.builtins.formatting.cljstyle, -- Clojure fmt
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.cmake_format,
 		null_ls.builtins.formatting.dfmt, -- D fmt
 		null_ls.builtins.formatting.elm_format,
-		null_ls.builtins.formatting.goimports,
-		null_ls.builtins.formatting.gofumpt,
+		null_ls.builtins.formatting.goimports, -- Go update imports
+		null_ls.builtins.formatting.gofumpt, -- Go fmt
 		null_ls.builtins.formatting.jq,
 		null_ls.builtins.formatting.lua_format,
 		null_ls.builtins.formatting.prettier, -- Typescript with prettier
+		null_ls.builtins.formatting.prettierd, -- yaml with prettier
 		null_ls.builtins.formatting.rustfmt,
 		null_ls.builtins.formatting.trim_whitespace, -- Remove junk whitespace
 		null_ls.builtins.formatting.zigfmt,
