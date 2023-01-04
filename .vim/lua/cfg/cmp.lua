7caa5784f5bed4c95c4234449fbc7ee6d5c126ca
Harley Laue <losinggeneration@gmail.com>
2023-01-04 15:31:04 -0800

    Some minor updates to cmp

--- .vim/lua/cfg/cmp.lua
+++ .vim/lua/cfg/cmp.lua
@@ -2,6 +2,11 @@
 local cmp = require("cmp")
 local snippy = require("snippy")
 
+local function has_words_before()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
 	preselect = cmp.PreselectMode.None,
 	snippet = {
@@ -32,10 +37,12 @@ cmp.setup({
 				snippy.expand()
 			elseif snippy.can_expand_or_advance() then
 				snippy.expand_or_advance()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
-		end, {"i", "s"}),
+		end, { "i", "s" }),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
@@ -44,7 +51,7 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end, {"i", "s"}),
+		end, { "i", "s" }),
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lua" },
@@ -57,6 +64,7 @@ cmp.setup({
 		-- { name = 'neosnippet' }, -- For neosnippet users.
 
 		{ name = "git" },
+		{ name = "buffer" },
 	}, {
 		{ name = "buffer" },
 	}),
