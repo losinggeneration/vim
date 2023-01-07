2b00d1cdc2ce5b749218db296b777726b80bc656
Harley Laue <losinggeneration@gmail.com>
2023-01-06 18:26:56 -0800

    Swap NERDTree for Neotree

---
+++ .vim/lua/cfg/neotree.lua
@@ -0,0 +1,3 @@
+-- Map F7 to toggle Neotree
+vim.keymap.set("n", "<F7>", ":Neotree toggle<CR>", {silent = true})
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
--- .vim/lua/plugins.lua
+++ .vim/lua/plugins.lua
@@ -291,8 +291,21 @@ return require("packer").startup(function()
 		"scrooloose/nerdcommenter",
 		{
 			"scrooloose/nerdtree",
+			disable = true,
 			config = function()
 				require("cfg.nerdtree")
+			end,
+		},
+		{
+			"nvim-neo-tree/neo-tree.nvim",
+			branch = "v2.x",
+			requires = {
+				"nvim-lua/plenary.nvim",
+				"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+				"MunifTanjim/nui.nvim",
+			},
+			config = function()
+				require("cfg.neotree")
 			end,
 		},
 		{
