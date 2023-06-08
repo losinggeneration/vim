e8263a7ccc345d284dcb3c16273ad5f858a6147d
Harley Laue <losinggeneration@gmail.com>
2023-06-08 13:43:21 -0700

    Add neovide specific config

--- .vim/init.lua
+++ .vim/init.lua
@@ -1,6 +1,7 @@
 require("cfg.settings")
 require("plugins")
 require("colors")
+require("cfg.neovide")
 
 vim.cmd("highlight TabS term=standout cterm=standout gui=standout")
 vim.cmd("highlight STabs term=standout cterm=standout gui=standout")
---
+++ .vim/lua/cfg/neovide.lua
@@ -0,0 +1,10 @@
+if vim.g.neovide then
+	vim.opt.guifont = { "Hack Nerd Font Mono", "h14" }
+	--vim.opt.guifont = { "Source Code Pro", "h14" }
+	vim.g.neovide_scale_factor = 0.6
+
+	vim.g.neovide_hide_mouse_when_typing = true
+
+	vim.g.neovide_refresh_rate = 60
+	vim.g.neovide_refresh_rate_idle = 1
+end
