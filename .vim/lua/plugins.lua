5c019530f5977cb192ca8188d420e054b3b710e1
Harley Laue <losinggeneration@gmail.com>
2023-07-31 13:57:16 -0700

    Switch neo-tree to version 3.x

--- .vim/lua/plugins.lua
+++ .vim/lua/plugins.lua
@@ -432,7 +432,7 @@ return require("packer").startup(function()
 		},
 		{
 			"nvim-neo-tree/neo-tree.nvim",
-			branch = "v2.x",
+			branch = "v3.x",
 			requires = {
 				"nvim-lua/plenary.nvim",
 				"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
