d7308ef45772840f77e682a25cf626db495da905
Harley Laue <losinggeneration@gmail.com>
2023-09-20 21:34:19 -0700

    Remove some unused Lua files

--- .vim/lua/cfg/nlspsettings.lua
+++
@@ -1,7 +0,0 @@
-require("nlspsettings").setup({
-	config_home = vim.fn.stdpath("config") .. "/nlsp-settings",
-	local_settings_dir = ".nlsp-settings",
-	local_settings_root_markers_fallback = { ".git" },
-	append_default_schemas = true,
-	loader = "json",
-})
