b4229a348b91eaa1bb76f777fbe450ace18134e9
Harley Laue <losinggeneration@gmail.com>
2023-10-11 02:48:13 -0700

    Add Neorg

--- .vim/lua/plugins/editing.lua
+++ .vim/lua/plugins/editing.lua
@@ -79,5 +79,25 @@ return {
 				om.setup(opts)
 			end,
 		},
+
+		{
+			"nvim-neorg/neorg",
+			build = ":Neorg sync-parsers",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			opts = {
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							default_workspace = "notes",
+							workspaces = {
+								notes = "~/Documents/notes/neorg",
+							},
+						},
+					},
+				},
+			},
+		},
 	},
 }
