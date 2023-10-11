83bcb5c70eafa73861e8b44ee6bc5f9e30e6fa60
Harley Laue <losinggeneration@gmail.com>
2023-10-11 03:14:52 -0700

    Add a couple useful Neorg modules

--- .vim/lua/plugins/editing.lua
+++ .vim/lua/plugins/editing.lua
@@ -88,6 +88,12 @@ return {
 				load = {
 					["core.defaults"] = {}, -- Loads default behaviour
 					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.summary"] = {},
+					["core.completion"] = {
+						config = {
+							engine = "nvim-cmp",
+						},
+					},
 					["core.dirman"] = { -- Manages Neorg workspaces
 						config = {
 							default_workspace = "notes",
