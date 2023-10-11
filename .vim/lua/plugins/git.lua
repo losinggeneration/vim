36afb6585e418e372098be75026dc3e794727e2c
Harley Laue <losinggeneration@gmail.com>
2023-10-11 02:49:12 -0700

    Resolve some Git key conflicts

--- .vim/lua/plugins/git.lua
+++ .vim/lua/plugins/git.lua
@@ -5,9 +5,24 @@ return {
 		"tpope/vim-fugitive",
 		cmd = { "G", "Gdiff" },
 		keys = {
-			{ "<Leader>Gd", ":Gdiff<cr>", desc = "Git diff" },
-			{ "<Leader>Gc", ":G commit<cr>", desc = "Git Commit" },
-			{ "<Leader>Gcv", ":G commit -v<cr>", desc = "Git Commit -v" },
+			{ "<Leader>gd", ":Gdiff<cr>", desc = "diff" },
+			{ "<Leader>gcc", ":G commit<cr>", desc = "commit" },
+			{ "<Leader>gcv", ":G commit -v<cr>", desc = "commit -v" },
+		},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			{ "<leader>gc", false }, -- disable git commits so it can be remapped
+			{ "<leader>gcl", "<cmd>Telescope git_commits<CR>", desc = "list" },
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		opts = {
+			defaults = {
+				-- ["<leader>gc"] = { name = "commits" },
+			},
 		},
 	},
 	{
@@ -16,7 +31,7 @@ return {
 
 		keys = {
 			{
-				"<space>tr",
+				"<space>gpt",
 				[[:lua require("octo.reviews.thread-panel").show_review_threads()<cr>]],
 				desc = "Show review threads",
 			},
