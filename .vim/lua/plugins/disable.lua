0aaa8c966fce16eae2b9555ad621b07798dd85cc
Harley Laue <losinggeneration@gmail.com>
2024-05-26 19:18:08 -0700

    Condense disabled plugins

--- .vim/lua/plugins/disable.lua
+++ .vim/lua/plugins/disable.lua
@@ -49,29 +49,14 @@ if vim.g.disable_lazy_plugins == true then
 end
 
 return {
-	{
-		"tpope/vim-sleuth",
-		-- This can cause some issues with file types not respecting the
-		-- desired global indentation. This also is what causes the
-		-- initial text when nvim is open to go away without input.
-		-- Because this also messes with the filetype, it can also
-		-- cause color scheme colors to not load correctly.
-		enabled = false,
-	},
-	{
-		"akinsho/bufferline.nvim",
-		enabled = false,
-	},
-	{
-		"echasnovski/mini.pairs",
-		enabled = false,
-	},
-	{
-		"echasnovski/mini.comment",
-		enabled = false,
-	},
-	{
-		"nvimtools/none-ls.nvim",
-		enabled = false,
-	},
+	-- slueth can cause some issues with file types not respecting the desired
+	-- global indentation. This also is what causes the initial text when nvim
+	-- is open to go away without input. Because this also messes with the
+	-- filetype, it can also cause color scheme colors to not load correctly.
+	{ "tpope/vim-sleuth", enabled = false },
+	{ "akinsho/bufferline.nvim", enabled = false },
+	{ "echasnovski/mini.pairs", enabled = false },
+	{ "echasnovski/mini.comment", enabled = false },
+	{ "nvimtools/none-ls.nvim", enabled = false },
+	{ "lualine.nvim", opts = { theme = "gruvbox-material" } },
 }
