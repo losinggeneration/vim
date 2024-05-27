a27c3fc1614bda7481c79265e59044a36930523e
Harley Laue <losinggeneration@gmail.com>
2024-05-26 19:17:15 -0700

    Switch to gruvbox-material

--- .vim/lua/plugins/colorscheme.lua
+++ .vim/lua/plugins/colorscheme.lua
@@ -1,10 +1,17 @@
--- add gruvbox
 return {
 	-- color schemes
 	{
 		--"gruvbox-community/gruvbox", -- VimScript standard
 		"ellisonleao/gruvbox.nvim", -- Treesitter highlighting
-		"sainnhe/gruvbox-material", -- VimScript + treesitter support with a softer pallete
+		{
+			"sainnhe/gruvbox-material", -- VimScript + treesitter support with a softer pallete
+			init = function()
+				vim.g.gruvbox_material_foreground = "original" -- material (default), mix, or original
+				-- vim.g.gruvbox_material_background = "medium" -- soft, medium (default), or hard
+				vim.g.gruvbox_material_enable_bold = 1
+			end,
+		},
+		"luisiacc/gruvbox-baby",
 		"tomasr/molokai",
 		"veloce/vim-aldmeris",
 		"bluz71/vim-moonfly-colors",
@@ -15,7 +22,7 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "gruvbox",
+			colorscheme = "gruvbox-material",
 		},
 	},
 }
